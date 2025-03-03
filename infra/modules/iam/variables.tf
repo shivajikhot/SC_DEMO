89034f3df2084d0599d77fa98a15670e5c504d0d
shivajikhot <shivajkv@gmail.com>
2025-03-03 12:01:45 +0530

    Create variables.tf

---
+++ infra/modules/iam/variables.tf
@@ -0,0 +1,9 @@
+variable "policy_name" {
+  description = "Name of the IAM policy"
+  type        = string
+}
+
+variable "role_name" {
+  description = "Name of the IAM role"
+  type        = string
+}
