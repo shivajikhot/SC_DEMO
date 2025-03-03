5785598f930eb263055da7285a712d6d29840f60
shivajikhot <shivajkv@gmail.com>
2025-03-03 12:00:57 +0530

    Create main.tf

---
+++ infra/modules/iam/main.tf
@@ -0,0 +1,69 @@
+# IAM Policy for Launch Role
+resource "aws_iam_policy" "ec2_servicecatalog_policy" {
+  name        = var.policy_name
+  description = "Policy to allow Service Catalog to create EC2 instances"
+  policy      = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Effect": "Allow",
+            "Action": [
+                "ec2:RunInstances",
+                "ec2:TerminateInstances",
+                "ec2:DescribeInstances",
+                "ec2:DescribeSubnets",
+                "ec2:DescribeVpcs",
+                "ec2:CreateSecurityGroup",
+                "ec2:AuthorizeSecurityGroupIngress",
+                "ec2:AuthorizeSecurityGroupEgress",
+                "ec2:AssociateIamInstanceProfile",
+                "iam:PassRole"
+            ],
+            "Resource": "*"
+        }
+    ]
+}
+POLICY
+}
+
+#  IAM Role for Launch Constraint
+resource "aws_iam_role" "servicecatalog_launch_role" {
+  name               = "var.role_name"
+  assume_role_policy = <<POLICY
+{
+    "Version": "2012-10-17",
+    "Statement": [
+        {
+            "Sid": "GivePermissionsToServiceCatalog",
+            "Effect": "Allow",
+            "Principal": {
+                "Service": "servicecatalog.amazonaws.com"
+            },
+            "Action": "sts:AssumeRole"
+        },
+        {
+            "Effect": "Allow",
+            "Principal": {
+                "AWS": "arn:aws:iam::575108922676:root"
+            },
+            "Action": "sts:AssumeRole",
+            "Condition": {
+                "StringLike": {
+                    "aws:PrincipalArn": [
+                        "arn:aws:iam::575108922676:role/TerraformEngine/TerraformExecutionRole*",
+                        "arn:aws:iam::575108922676:role/TerraformEngine/ServiceCatalogExternalParameterParserRole*",
+                        "arn:aws:iam::575108922676:role/TerraformEngine/ServiceCatalogTerraformOSParameterParserRole*"
+                    ]
+                }
+            }
+        }
+    ]
+}
+POLICY
+}
+
+resource "aws_iam_role_policy_attachment" "attach_policy" {
+  role       = aws_iam_role.servicecatalog_launch_role.name
+  policy_arn = aws_iam_policy.ec2_servicecatalog_policy.arn
+}
