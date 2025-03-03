7f58f6a4b98fe7b580a2b35dec24c895fb869541
shivajikhot <shivajkv@gmail.com>
2025-03-03 11:40:27 +0530

    Create variables.tf

---
+++ infra/modules/service_catalog/variables.tf
@@ -0,0 +1,44 @@
+variable "portfolio_name" {
+  description = "Name of the Service Catalog portfolio"
+  type        = string
+}
+
+variable "portfolio_description" {
+  description = "Portfolio description"
+  type        = string
+}
+
+variable "provider_name" {
+  description = "Provider name"
+  type        = string
+}
+
+variable "product_name" {
+  description = "Service Catalog Product Name"
+  type        = string
+}
+
+variable "product_owner" {
+  description = "Owner of the Service Catalog product"
+  type        = string
+}
+
+variable "product_description" {
+  description = "Description of the Service Catalog product"
+  type        = string
+}
+
+variable "artifact_version" {
+  description = "Version of the provisioning artifact"
+  type        = string
+}
+
+variable "template_url" {
+  description = "S3 URL for Terraform artifact"
+  type        = string
+}
+
+variable "launch_role_arn" {
+  description = "IAM Role ARN for Service Catalog Launch Constraint"
+  type        = string
+}
