80c8baf4425eecb40bce169903eccf81d7b4931c
shivajikhot <shivajkv@gmail.com>
2025-03-05 17:57:04 +0530

    Update main.tf

--- infra/env/dev/main.tf
+++ infra/env/dev/main.tf
@@ -18,13 +18,7 @@ module "service_catalog" {
   portfolio_name        = "EC2 Portfolio"
   portfolio_description = "Portfolio for Terraform configurations"
   provider_name         = "IT (it@example.com)"
-  product_name          = "EC2_instanace"
-  product_owner         = "Devops"
-  product_description   = "Terraform product containing an Amazon EC2."
-  support_url           = "https://wiki.example.com/IT/support"
-  support_email         = "ITSupport@example.com" 
-  artifact_version      = "v1.0"
-  template_url          = "https://terraform-backend-statefi.s3.us-west-1.amazonaws.com/service-catalog_ec2.tar.gz"
+  products              = var.products
   launch_role_arn       = module.iam.iam_role_arn
   iam_group_arn         = module.iam_group.iam_group_arn
 }
