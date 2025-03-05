0d9f8a09403eb3d95c5483c825f575959d80bef3
shivajikhot <shivajkv@gmail.com>
2025-03-05 17:45:35 +0530

    Update main.tf

--- infra/modules/service_catalog/main.tf
+++ infra/modules/service_catalog/main.tf
@@ -5,25 +5,29 @@ resource "aws_servicecatalog_portfolio" "portfolio" {
 }
 
 resource "aws_servicecatalog_product" "product" {
-  name                = var.product_name
-  owner               = var.product_owner
-  description         = var.product_description
+  for_each = var.products
+
+  name                = each.value.name
+  owner               = each.value.owner
+  description         = each.value.description
   type                = "EXTERNAL"
-  support_email       = var.support_email
-  support_url         = var.support_url
+  support_email       = each.value.support_email
+  support_url         = each.value.support_url
 
   provisioning_artifact_parameters {
-    name         = var.artifact_version
+    name         = each.value.artifact_version
     description  = "Initial version"
     type         = "EXTERNAL"
-    template_url = var.template_url
+    template_url = each.value.template_url
     disable_template_validation = true
   }
 }
 
 resource "aws_servicecatalog_product_portfolio_association" "product_association" {
+  for_each = aws_servicecatalog_product.product
+
   portfolio_id = aws_servicecatalog_portfolio.portfolio.id
-  product_id   = aws_servicecatalog_product.product.id
+  product_id   = each.value.id
 }
 
 resource "aws_servicecatalog_principal_portfolio_association" "portfolio_access_group" {
@@ -33,12 +37,14 @@ resource "aws_servicecatalog_principal_portfolio_association" "portfolio_access_
 }
 
 resource "aws_servicecatalog_constraint" "launch_constraint" {
+  for_each = aws_servicecatalog_product.product
+
   portfolio_id = aws_servicecatalog_portfolio.portfolio.id
-  product_id   = aws_servicecatalog_product.product.id
+  product_id   = each.value.id
   type         = "LAUNCH"
 
   parameters = jsonencode({
     "RoleArn" = var.launch_role_arn
   })
- depends_on = [var.launch_role_arn]
 }
+
